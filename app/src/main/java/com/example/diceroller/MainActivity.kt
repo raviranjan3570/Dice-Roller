c6af83dc944a178b0d1c990d15fe8f33720b07a8
Ravi Ranjan <raviranjan3570@gmail.com>
2021-01-19 22:56:16 +0530

    added dice functionality

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -2,10 +2,25 @@ package com.example.diceroller
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val rollButton : Button = findViewById(R.id.roll_button)
+        rollButton.setOnClickListener {
+            rollDice()
+        }
+    }
+
+    private fun rollDice() {
+
+        val resultTextView : TextView = findViewById(R.id.result_tv)
+        val randomNumber : Int =  Random.nextInt(6) + 1
+        resultTextView.text = randomNumber.toString()
     }
 }
